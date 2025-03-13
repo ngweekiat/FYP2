c3861533831fdc1b82233d883a9d369862c14e76
ngweekiat <75771397+ngweekiat@users.noreply.github.com>
2025-03-13 16:25:45 +0800

    Removed android duplicate - no time for refactoring

--- frontend/android - Copy/app/src/main/java/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fyp_androidapp
-
-object Constants {
-    const val BACKEND_IP = "172.20.10.3" // Change only this
-    const val BASE_URL = "http://$BACKEND_IP:3000/api"
-}
--- frontend/android - Copy/app/src/main/java/com/example/fyp_androidapp/data/models/TableItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fyp_androidapp.data.models
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class TableItem(
-    val label: String,
-    val icon: ImageVector,
-    val route: String
-)
